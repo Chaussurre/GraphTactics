ad75c3cc59e1f5de8851081695c7e6fcc58415f0
Chaussurre <Samuel@Corno.com>
2021-04-07 08:34:59 +0200

    [FEATURE] Basic Node created

---
+++ Assets/Graph/Node/DisplayArmy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteAlways, RequireComponent(typeof(Node))]
+public class DisplayArmy : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Text text = GetComponentInChildren<Text>();
+        if (text == null)
+            return;
+
+        Node node = GetComponent<Node>();
+        text.text = node.GetArmySize().ToString();
+    }
+}
---
+++ Assets/Graph/Node/Node.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    [SerializeField, Range(0, 100)]
+    int armySize;
+
+    public int GetArmySize()
+    {
+        return armySize;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
